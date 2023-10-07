c6c4591e0e0e7149a3e1f06951288729dabdad65
WheretIB <wheretib@gmail.com>
2023-10-07 22:11:49 +0300

    Revert comment change

--- VM/src/ludata.h
+++ VM/src/ludata.h
@@ -7,7 +7,7 @@
 // special tag value is used for user data with inline dtors
 #define UTAG_IDTOR LUA_UTAG_LIMIT
 
-// special tag value is used for newproxy-created user data
+// special tag value is used for newproxy-created user data (all other user data objects are host-exposed)
 #define UTAG_PROXY (LUA_UTAG_LIMIT + 1)
 
 #define sizeudata(len) (offsetof(Udata, data) + len)
